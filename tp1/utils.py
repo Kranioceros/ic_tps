97499668caef596126973913169428860526e713
Ramiro Garay <ramiro.sgaray@gmail.com>
2020-09-08 11:26:02 -0300

    Agregar funcion para particionar datos de entrenamiento

--- tp1/utils.py
+++ tp1/utils.py
@@ -30,3 +30,33 @@ def extender(m):
     (fil, _col) = m.shape
     col0 = np.ones((fil, 1)) * (-1)
     return np.hstack((col0, m))
+
+# Crea `n` particiones con un porcentaje `p` de patrones de entrenamiento y 1-`p` de pruebas
+# `p` es un valor entre 0 y 1
+# La entrada de datos es `m`
+# Devuelve un arreglo de `n` particiones
+# Cada particion es una tupla de indices de la forma:
+# (idx_entr, idx_prueba)
+# donde cada idx es un vector con los indices de las filas correspondientes en `m`
+# Si random es True, los vectores de indices son elegidos aleatoriamente
+
+
+def particionar(m, n, p, random=False):
+    (fils, cols) = m.shape
+    particiones = []
+
+    for _i in range(n):
+        # Idx contiene los indices de todas las filas
+        idx = np.arange(fils)
+        # Los mezclamos si random=True
+        if random:
+            np.random.shuffle(idx)
+        # Calculamos el numero de patrones de entrenamiento en base a `p`
+        Ne = int(np.floor(fils*p))
+        # Agregamos los indices al arreglo de particiones
+        idx_entr = idx[:Ne]
+        idx_prueba = idx[Ne:]
+
+        particiones.append((idx_entr, idx_prueba))
+
+    return particiones
