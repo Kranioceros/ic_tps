6e96768fd8a7e16d9e677d16819479c2574bd189
Ramiro Garay <ramiro.sgaray@gmail.com>
2020-09-08 11:26:24 -0300

    Agregar script para ejercicio 2a (vacio)

---
+++ tp1/ej2a.py
@@ -0,0 +1,11 @@
+import numpy as np
+from utils import particionar
+
+
+def main():
+    pass
+
+
+# Ejecutar la funcion main solo si el script es ejecutado directamente
+if __name__ == "__main__":
+    main()
