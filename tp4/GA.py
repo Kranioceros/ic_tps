e071446267fd2b3c52c4ec2473b65260eedf5ce3
Estanislao Claucich <eclaucich@hotmail.com>
2020-10-31 11:52:13 -0300

    Ejercicio 1 - falta gradiente descendiente

--- tp4/GA.py
+++ tp4/GA.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 from DNA import DNA
 #Clase que controla el funcionamiento de algoritmo genetico
 #Cosas necesarias:
@@ -25,61 +26,93 @@ class GA:
 
     #Inicializo la poblacion de N agentes al azar
     def Initialize(self):
-        #Para N agentes
+        #Inicializo N agentes
         for _i in range(self.N):
            self.population.append(DNA(self.n))
 
 
-    def Evolve(self):
+    #Controla la logica del algoritmo genetico
+    #elitismo -> quedarse con el mejor de cada generacion
+    #convGen -> si durante esta cantidad seguida de genraciones se repite el mismo bestFitness -> terminar
+    def Evolve(self, elitismo=False, convGen = 10):
+
+        #Cuenta cuantas veces se repite el mejor fitness (en generaciones seguidas)
+        bestRepeated = 0
+
+        #El mejor fitness de la generacion anterior
+        #TODO: inicializarlo de manera mas inteligente
+        bestFitnessPrev = -9999
+
         #Itero tantas veces como generaciones maximas
         for _i in range(self.maxGens):
             #Poblacion nueva, me voy guardando los nuevos agentes
             newPopulation = []
 
-            #Evalua poblacion y me guardo los fitness
-            v_fitness = self.EvaluatePopulation()
+            #Evaluo poblacion actual y me guardo los fitness y el mejor
+            (v_fitness, bestFitnessActual) = self.EvaluatePopulation()
 
-            #Muestro el fitness medio para esta generacion
-            print(f"Max Fitness generacion {_i}: {np.max(v_fitness)} | ({self.f_deco(self.population[np.argmax(v_fitness)].dna)}, {-self.f_fitness(self.f_deco(self.population[np.argmax(v_fitness)].dna))})")
+            #El mejor agente de esta poblacion
+            bestAgent = self.population[np.argmax(v_fitness)]
+
+            #Muestro el mejor fitness para esta generacion
+            print(f"Coord Minimas: ({self.f_deco(bestAgent.dna)}, {-self.f_fitness(self.f_deco(bestAgent.dna))})")
             #print(f"max: {np.max(v_fitness)} | mean: {np.mean(v_fitness)} | STD: {np.std(v_fitness)}")
 
+            #Verifico si se repite el mejor fitness
+            if(bestFitnessActual == bestFitnessPrev):
+                bestRepeated += 1
+                #Si ya se repitió convGen veces -> termino
+                if(bestRepeated == convGen):
+                    print(f"Convergencia por repeticion en generacion {_i}")
+                    break
+            else:
+                #Verifico si el mejor actual es mejor que el anterior
+                if(bestFitnessActual > bestFitnessPrev):
+                    bestFitnessPrev = bestFitnessActual
+                    bestRepeated = 0
+
+            #Ordeno los fitness (solo sirve para el picker ventanas)
+            #TODO: sacar esto si no usamos picker ventanas
             v_fitness_ord = -np.sort(-v_fitness)
 
+            #Si hago elitismo me quedo con el mejor de la generacion
+            if(elitismo):
+                newPopulation.append(bestAgent)
+
+            #Solo para picker ventanas
+            #TODO:sacar si no usamos picker ventanas
             i = self.N
+
             #Itero tantas veces como agentes en una poblacion
-            for _j in range(int(np.ceil(self.N/2))):
+            for _j in range(int(self.N/2)):
+
+                #Picker recipiente
                 #Eligo dos agentes al "azar"
                 a1 = self.Picker()
                 a2 = self.Picker()
 
+                #Picker ventanas
                 #a1 = self.PickerWindow(i, v_fitness_ord)
                 #i -= 1
                 #a2 = self.PickerWindow(i, v_fitness_ord)
 
-                #print(f"Padres: {a1.dna} | {a2.dna}")
-
                 #Combino los dos agentes y obtengo dos nuevos
                 (newAgent1, newAgent2) = a1.CrossOver(a2, self.probCrossOver)
-
-                #print(f"Hijoss: {newAgent1.dna} | {newAgent2.dna}")
 
                 #Muto a los agentes nuevos
                 newAgent1.Mutate(self.probMutation)
                 newAgent2.Mutate(self.probMutation)
 
-                #print(f"Mutado: {newAgent1.dna} | {newAgent2.dna}")
-
-                #print(f"{np.logical_xor(a1.dna, newAgent1.dna)} | {np.logical_xor(a2.dna, newAgent2.dna)}")
-
                 #Los agrego a la nueva poblacion
                 newPopulation.append(newAgent1)
+
                 #Si N es impar, en la ultima iteracion del for tengo que agregar un solo padre
                 if(len(newPopulation) == self.N):
                     break
+
                 newPopulation.append(newAgent2)
 
             #Una vez generados N agentes nuevos, reemplazo la poblacion actual
-            #TODO: revisar si esta asignacion no causa problemas
             self.population = list(newPopulation)
 
             #print(f"Generacion {_i}")
@@ -90,7 +123,6 @@ class GA:
     #Se va vaciando un recipiente de volumen 1 hasta que llegue a 0 (o menor)
     #El agente que haga que el volumen caiga de 0 es el elegido
     #Se usa el fitness normalizado para ir restando
-    #TODO: seguro hay una forma de seleccionar un elemento al azar de un vector, en vez de agarrar un indice y despues usarlo
     def Picker(self):
         #Comienzo con el recipiente lleno
         volume = 1.0
@@ -112,6 +144,9 @@ class GA:
         #Devuelvo el agente que vació el recipiente
         return self.population[randAgent]
 
+
+    #Seleccion de agente mediante ventanas
+    #TODO: terminarlo
     def PickerWindow(self, i, v_fitness, v_idxs):
         ventana = v_fitness[0:i]
 
@@ -123,7 +158,6 @@ class GA:
 
     #Evalúa cada agente de la poblacion con la funcion de fitness
     #Tambien normaliza todos los fitness
-    #TODO: habrá alguna forma de no recorrer dos veces el for??
     def EvaluatePopulation(self):
         #Vector de fitness
         v_fitness = np.zeros(self.N)
@@ -135,14 +169,16 @@ class GA:
             a.fitness = v_fitness[i]
 
         #Busco el mejor fitness
-        bestFitness = np.max(v_fitness)
+        bestFitness = np.max(v_fitness) #'b'
+        worstFitness = np.min(v_fitness) #'a'
 
+        #Mapeo [a,b] -> [0,1]
         #Normalizo los fitness de cada agente con respecto al fitness maximo
         for a in self.population:
-            a.fitnessNormalize = a.fitness/np.abs(bestFitness)
+            a.fitnessNormalize = (a.fitness-worstFitness)/(bestFitness-worstFitness)
 
         #Devuelvo el fitness solo como debug, no es necesario devolverlo
-        return v_fitness
+        return (v_fitness, bestFitness)
 
 
     #Funcion debug para imprimir todos los agentes de la poblacion
--- tp4/main.py
+++ tp4/main.py
@@ -4,23 +4,36 @@ from GA import GA
 def main():
 
     N = 100 #Cantidad de agentes en una poblacion
-    n = 20 #Cantidad de alelos en cada agente
+    n = 30 #Cantidad de alelos en cada agente
     probCrossOver = 0.9 #Probabilidad de que haya cruza
     probMutation = .1 #Probabilidad de que un alelo se mute
     maxGens = 5000 #Cantidad maxima de generacion a iterar
 
-    #Creo el controlador de la poblacion
-    ga = GA(N, n, probCrossOver, probMutation, DecoDecimalDoble, fitness3, maxGens)
+    #Pares de decodificadores/fitness para cada funcion
+    ecuacion_deco = [(DecoDecimal, fitness1),(DecoDecimal, fitness2), (DecoDecimalDoble, fitness3)] 
 
-    #print("Primera poblacion")
-    #ga.DebugPopulation()
+    #Cambiar esta variable para cambiar de ecuacion
+    #Para ec=0 y ec=1 cambiar los extremos [a,b] de la funcion DecoDecimal
+    #TODO: pensar una forma mas comoda de cambiar entre ecuaciones
+    # Numero de la ecuacion usada (0,1,2)
+    ec = 0
+
+    #Funcion de decodificacion y fitness a usar
+    deco = ecuacion_deco[ec][0]
+    fitn = ecuacion_deco[ec][1]
+
+    #Creo el controlador de la poblacion
+    ga = GA(N, n, probCrossOver, probMutation, deco, fitn, maxGens)
 
     #Evoluciono
-    ga.Evolve()
+    #convGen -> si durante convGen generaciones seguidas se repite el mejor fitness -> termino
+    ga.Evolve(elitismo=True, convGen=10)
 
-    #print("Ultima poblacion")
-    #ga.DebugPopulation()
 
+
+
+
+# --------------- Funciones Fitness --------------------
 
 #Funcion de fitness de prueba, crece segun la cantidad de 1s
 def NumberOfOnes(v):
@@ -30,26 +43,32 @@ def NumberOfOnes(v):
             fitness += 10
     return fitness
 
-def DecoIdentidad(v):
-    return (v,v)
-
-
+# Ecuacion 1 - N=100, n=30, elitismo=True, convGen=10
 def fitness1(x):
     y = -x*np.sin(np.sqrt(np.abs(x)))
     return -y
 
+# Ecuacion 2 - N=100, n=30, elitismo=True, convGen=10
 def fitness2(x):
     y = x + 5*np.sin(3*x) + 8*np.cos(5*x)
     return -y
 
+# Ecuacion 3 - N=300, n=50, elitismo=True, convGen=30
 def fitness3(xy):
     x = xy[0]
     y = xy[1]
 
     z = (x**2 + y**2)**0.25 * (np.sin(50*(x**2+y**2)**0.1)**2 + 1)
-    return z
+    return -z
 
 
+#--------------- Decodificadores -------------------
+
+#Devuelve lo que recibe, porque el fitness de este deco necesita el vector binario
+def DecoIdentidad(v):
+    return v
+
+#Decodificador binario-decimal de una variable
 def DecoDecimal(v, a=0, b=20):
     k = len(v)
     d = sum(2**(k-np.array(range(1,k+1)))*v)
@@ -58,6 +77,7 @@ def DecoDecimal(v, a=0, b=20):
 
     return x
 
+#Decodificador binario-decimal de dos variables
 def DecoDecimalDoble(v, a=-100, b=100):
     k = int(len(v)/2)
 
@@ -68,6 +88,8 @@ def DecoDecimalDoble(v, a=-100, b=100):
     y = a + (d2*((b-a)/((2**k)-1)))
 
     return (x,y)
+
+
 
 if __name__ == "__main__":
     main()
